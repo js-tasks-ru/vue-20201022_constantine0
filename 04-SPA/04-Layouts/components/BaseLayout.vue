832190ac8eaf0225f70393e9c9107cc485223a93
constantine <constantine@squla.com>
2020-11-14 10:46:29 +0100

    add solution for 04-spa Layouts

--- 04-SPA/04-Layouts/components/BaseLayout.vue
+++ 04-SPA/04-Layouts/components/BaseLayout.vue
@@ -1,13 +1,32 @@
-<template></template>
+<template>
+  <div>
+    <the-header />
+    <router-view />
+    <the-footer />
+  </div>
+</template>
 
 <script>
+import TheHeader from './TheHeader';
+import TheFooter from './TheFooter';
+
 export default {
   name: 'BaseLayout',
+
+  components: {
+    TheHeader,
+    TheFooter,
+  },
 };
 </script>
 
 <style scoped>
 main {
   flex: 1 0 auto;
+}
+.page.page_onboarding {
+  max-width: 374px;
+  width: 100%;
+  margin: 0 auto;
 }
 </style>
--- 04-SPA/04-Layouts/components/TheFooter.vue
+++ 04-SPA/04-Layouts/components/TheFooter.vue
@@ -1,9 +1,29 @@
-<template></template>
+<template>
+  <footer class="footer">
+    <div class="container">
+      {{ copyright }}
+      <a :href="href">{{ title }}</a>
+    </div>
+  </footer>
+</template>
 
 <script>
 export default {
   name: 'TheFooter',
+
+  data() {
+    return {
+      title: 'Vue.js Course',
+      href: 'https://learn.javascript.ru/courses/vue',
+      copyright: `© ${new Date().getFullYear()}. learn.javascript.ru`,
+    };
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.footer {
+  padding: 100px 0 60px;
+  text-align: center;
+}
+</style>
